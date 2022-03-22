4ee1b5fcdbef5bc593bae927528e8348f0a8e09e
Cosmin Home <cosminsign@outlook.com>
2022-03-22 22:12:05 +0200

    Exceptions - third commit

--- src/main/java/org/example_5/Main.java
+++ src/main/java/org/example_5/Main.java
@@ -34,8 +34,26 @@ public class Main {
         }
         return listtoreturn;
 
-
     }
+
+    public static Student checkFirstLastName(String FirstName, String LastName, List<Student> myList) {
+        for (Student s : myList) {
+            if (s.getFirstName().equals(FirstName) || s.getLastName().equals(LastName)) {
+                return s;
+            }
+        }
+        throw new NoSuchElementException("Student First or/and Last name are empty");
+    }
+
+    public static Student checkGender(String Gender, List<Student> myList) {
+        for (Student s : myList) {
+            if (s.getGender().equalsIgnoreCase("male") || s.getGender().equalsIgnoreCase("female")) {
+                return s;
+            }
+        }
+        throw new NoSuchElementException("Gender not recognized");
+    }
+
 
     public static void main(String[] args) {
         //Adding student
@@ -58,7 +76,7 @@ public class Main {
             System.out.println(s.toString());
         }
 
-        //C.Delete student (by ID)
+        //C.Remove student (by ID)
         // Change the ID to see the exception
         myList.remove(removeByID("1800101415063", myList));
         System.out.println("After removing by ID:1800101415063 ");
@@ -80,8 +98,22 @@ public class Main {
             System.out.println(s.toString());
         }
 
+        //E.Verify Gender
+        System.out.println("Gender verify");
+        Student checkgender = checkGender("male", myList);
+        for (Student s : myList) {
+            System.out.println(s.toString());
+        }
+        //F.Verify Fist-Last Name String
+        System.out.println("First-Last name check");
+
+        Student checkname = checkFirstLastName("Kim", "Thomas", myList); //get the message because it was removed at point C. (with ID)
+        for (Student s : myList) {
+            System.out.println(s.toString());
+        }
     }
 }
+
 
 
 
